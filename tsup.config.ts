4c5468df3b9bde8ae0484b3614aadec4104e0df7
hassan <hsn.moradnezhad@gmail.com>
2026-07-17 13:14:02 +0330

    chore: set minify config

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   dts: true,
+  minify: true,
   sourcemap: false,
   clean: true,
   external: ["@tanstack/react-query"],
